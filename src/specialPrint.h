754f5ef79a8248a5bd7e519ac12a4df3792653fd
Eyre <18782835727@163.com>
2024-12-30 13:33:54 +0800

    界面打印更新

---
+++ src/specialPrint.h
@@ -0,0 +1,26 @@
+//
+// Created by CQL on 24-12-28.
+//
+
+#ifndef SPECIALPRINT_H
+#define SPECIALPRINT_H
+
+#include<bits/stdc++.h>
+#include<windows.h>
+#include<cstdlib>
+using namespace std;
+
+
+void gotoxy(int x, int y);
+
+int randint_range(int n, int m);
+
+void loading(string str, int width, int dash);
+
+void SlowPrint(string str, int width, int align);
+
+void MenuPrint(string str, int width);
+
+void openPrint();
+
+#endif //SPECIALPRINT_H
